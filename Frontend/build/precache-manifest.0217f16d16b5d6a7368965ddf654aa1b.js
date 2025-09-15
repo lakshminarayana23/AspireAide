1b11bd88f0e2e12a63c5d823dad82b6a801f2f6b
Lakshmi Narayana Boddula <luckyboddula@gmail.com>
2025-09-15 14:56:35 +0530

    Force add Frontend folder

---
+++ Frontend/build/precache-manifest.0217f16d16b5d6a7368965ddf654aa1b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2718dc5b7b708072dd4ffd8ced4c67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b702ac06ab471f6a3f3a",
+    "url": "/static/css/2.83d756e6.chunk.css"
+  },
+  {
+    "revision": "af0def5ab02812c6a869",
+    "url": "/static/css/main.254675e7.chunk.css"
+  },
+  {
+    "revision": "b702ac06ab471f6a3f3a",
+    "url": "/static/js/2.085b670e.chunk.js"
+  },
+  {
+    "revision": "8c3df7995d7ac410f963",
+    "url": "/static/js/3.dc9fb26f.chunk.js"
+  },
+  {
+    "revision": "af0def5ab02812c6a869",
+    "url": "/static/js/main.8ca04235.chunk.js"
+  },
+  {
+    "revision": "ca146662dd9ee0d060e6",
+    "url": "/static/js/runtime~main.b1168be7.js"
+  },
+  {
+    "revision": "61c6357b5e2bc0c31ffa353b8f22df64",
+    "url": "/static/media/scholarship.61c6357b.jpg"
+  },
+  {
+    "revision": "3c3b44978896ac1b1130883022d5060f",
+    "url": "/static/media/userLogin.3c3b4497.jpg"
+  },
+  {
+    "revision": "ff42254744abcc2371927d07628e83ef",
+    "url": "/static/media/userRegister.ff422547.jpg"
+  },
+  {
+    "revision": "3ac239f4904773005fb8cf47e61014fa",
+    "url": "/static/media/userRegistration.3ac239f4.jpg"
+  }
+]);
